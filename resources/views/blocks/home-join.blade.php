25318d19458caf6bc30a0a496636ac745a21ebeb
John Puddephatt <john@jdp.org.uk>
2023-12-17 12:08:38 +0000

    add movement link

---
+++ resources/views/blocks/home-join.blade.php
@@ -0,0 +1,23 @@
+<div class="bg-pink py-24 text-center text-white">
+    <div class="container">
+        <h3 class="mb-8 font-serif text-6xl uppercase leading-[0.85] md:text-7xl lg:text-8xl">Join the
+            movement</h3>
+
+        <p class="mx-auto mb-16 max-w-3xl text-xl 2xl:text-2xl">
+            Be part of the conversation by filling in our form and signing up to our mailing list for ways to get
+            involved, campaign news and events</p>
+
+        <a target="_blank" class="inline-flex flex-row bg-white text-xl text-black 2xl:text-2xl"
+            href="https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAANAAWUPEb1URUwxWEwwTE5WVzYyN1o5SktGS1Y4N1RKOC4u">
+            <span class="inline-block p-6">Take me to the form</span>
+
+            <span class="inline-block border-l-4 border-pink p-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="inline-block h-10 w-10 text-pink">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                </svg>
+
+            </span>
+        </a>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
     <div x-data="{ currentSection: null, backgroundColor: 'bg-pink' }" @newsection="currentSection = $event.detail.id, backgroundColor = $event.detail.color"
         :class="backgroundColor" class="overflow-hidden transition duration-1000">
 
-
         @include('blocks.home-hero')
         @include('blocks.home-about')
         @include('blocks.home-memories')
+        @include('blocks.home-join')
 
     </div>
 @endsection
