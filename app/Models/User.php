dedb54e90c5e6c624196c0f7c9e2a9702c1bfb25
John Puddephatt <john@jdp.org.uk>
2024-01-04 13:27:15 +0000

    enable password reset

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use Filament\Models\Contracts\FilamentUser;
+
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -51,7 +52,8 @@ class User extends Authenticatable implements FilamentUser
         'password' => 'hashed',
     ];
 
-    public function memories() {
+    public function memories()
+    {
         return $this->hasMany(\App\Models\Memory::class);
     }
 }
