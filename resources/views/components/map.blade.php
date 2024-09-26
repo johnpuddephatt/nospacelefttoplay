c5857df9b132526cb8c060acab9d832daaece35f
John Puddephatt <john@jdp.org.uk>
2024-09-26 12:48:06 +0100

    aspect

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -1,4 +1,4 @@
 <div class="full-width my-12 bg-green pb-16">
     <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1VlS64dYCrgHtDOI7jNrBiSsk8AD6Bfs&ehbc=015a2a"
-        class="aspect-[2.5] w-full"></iframe>
+        class="aspect[1] w-full md:aspect-[1.5] lg:aspect-[2.5]"></iframe>
 </div>
